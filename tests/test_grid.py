5ba9b066cca3fb469b80f38d2acacdbd03199ca6
maximt <vbmaxim@gmail.com>
2024-06-28 14:17:35 +0700

    test_get_tile_size

--- tests/test_grid.py
+++ tests/test_grid.py
@@ -1,6 +1,7 @@
 import pytest
+from unittest.mock import patch
 
-from ..libs.grid import get_grid_size
+from ..libs.grid import get_grid_size, get_tile_size
 
 
 @pytest.mark.parametrize("files_count, expected", [
@@ -26,3 +27,21 @@ def test_get_grid_size_capacity():
         x, y = get_grid_size(files_count)
         grid_count = x * y
         assert grid_count >= files_count
+
+
+def test_get_tile_size():
+    def mock_get_image_size(file_path: str) -> tuple[int, int]:
+        sizes = {
+            'image1.png': (500, 500),
+            'image2.png': (500, 800),
+            'image3.png': (800, 500),
+        }
+        return sizes.get(file_path, (0, 0))
+
+    files = ['image1.png', 'image2.png', 'image3.png']
+    expected_tile_size = (800, 800)
+
+    with patch('imagesgrid.libs.grid.get_image_size', side_effect=mock_get_image_size):
+        tile_size = get_tile_size(files)
+
+    assert tile_size == expected_tile_size
